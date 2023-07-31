4ee15e6a0163973f63b2d6cd8fe2f92fea65a484
Ilya Stasko <ilya.stasko@office.e2e4gu.ru>
2023-07-31 22:49:59 +0700

    implemented animation of rows with cards

--- app/src/main/java/com/example/loyaltycardwallet/ui/AnimatedCardsActivity.kt
+++ app/src/main/java/com/example/loyaltycardwallet/ui/AnimatedCardsActivity.kt
@@ -1,13 +1,16 @@
 package com.example.loyaltycardwallet.ui
 
+import android.animation.AnimatorSet
+import android.animation.ValueAnimator
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
+import androidx.core.view.forEachIndexed
 import androidx.core.view.get
 import androidx.core.view.size
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +19,10 @@ import com.example.loyaltycardwallet.R
 import com.example.loyaltycardwallet.databinding.ActivityAnimatedCardsBinding
 import com.example.loyaltycardwallet.di.AppComponent
 import com.example.loyaltycardwallet.model.InputCard
+import com.example.loyaltycardwallet.utils.dpToPx
+import com.example.loyaltycardwallet.utils.getScreenWidth
+
+const val DEFAULT_ANIMATION_DURATION = 4_000L
 
 class AnimatedCardsActivity : AppCompatActivity() {
 
@@ -23,11 +30,6 @@ class AnimatedCardsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAnimatedCardsBinding
 
     private lateinit var animatedCardsViewModel: AnimatedCardsViewModel
-
-    //
-    private lateinit var firstRow: LinearLayout
-    private lateinit var secondRow: LinearLayout
-    private lateinit var thirdRow: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,9 +47,16 @@ class AnimatedCardsActivity : AppCompatActivity() {
                     val cardView = (binding.cardsLayout[i] as ViewGroup)[j]
                     val position = (binding.cardsLayout.size * i) + j
                     initCard(cardView, position, randomCards[position])
-                    Log.d("AnimatedCardsActivity", "shop name = ${randomCards[position].shopName}")
                 }
             }
+
+            // layout width is 3 cards of 150dp + the distance between the cards (2 * 10 dp) + screen offset (5dp)
+            val layoutWidth = 475 // in dp
+
+            // x1 and x2 set the X coordinate positions between which the animation will take place
+            val x1 = (layoutWidth.dpToPx() - getScreenWidth()).toFloat()
+            val x2 = 5.dpToPx().toFloat()
+            startAnimation(x1, x2)
         }
 
         animatedCardsViewModel.getInputCards()
@@ -61,4 +70,38 @@ class AnimatedCardsActivity : AppCompatActivity() {
         cardImageView.setImageResource(resources.getIdentifier(cardImageName, "drawable", packageName))
     }
 
+    private fun startAnimation(x1: Float, x2: Float) {
+        val animators = mutableListOf<ValueAnimator>()
+        binding.cardsLayout.forEachIndexed { index: Int, row: View ->
+            val animator = if ((index + 1) % 2 != 0) { // animate 1st and 3rd rows
+                initRowAnimator(row as ViewGroup, -x1, x2)
+            } else { // animation 2nd row
+                initRowAnimator(row as ViewGroup, x2, -x1)
+            }
+            animators.add(animator)
+        }
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(animators.toList())
+        animatorSet.duration = DEFAULT_ANIMATION_DURATION
+        animatorSet.interpolator = AccelerateDecelerateInterpolator()
+        animatorSet.start()
+    }
+
+    // init ValueAnimator for rows
+    private fun initRowAnimator(row: ViewGroup, start: Float, end: Float): ValueAnimator {
+        val animator = ValueAnimator.ofFloat(start, end)
+        animator.addUpdateListener {
+            val value = it.animatedValue as Float
+            animateRow(row, value)
+        }
+        animator.repeatCount = ValueAnimator.INFINITE
+        animator.repeatMode = ValueAnimator.REVERSE
+        return animator
+    }
+
+    private fun animateRow(row: ViewGroup, value: Float) {
+        row.forEach { cardView: View ->
+            cardView.translationX = value
+        }
+    }
 }
